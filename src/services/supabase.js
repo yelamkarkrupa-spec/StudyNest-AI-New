36d0126df02c1739fd01b5f37717fdaa7c192f93
Krupa <yelamkarkrupa@gmail.com>
2026-07-09 23:10:30 +0530

    Update project without .env

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,5 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
+console.log(import.meta.env.VITE_SUPABASE_URL);
+console.log(import.meta.env.VITE_SUPABASE_ANON_KEY);
+
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
